661f7ed6a61184e19473f62b18d4f8381dc58f6e
Michael Purcella <mjpurcella@gmail.com>
2025-02-09 01:09:14 -0800

    change prettier rule

--- src/components/HamburgerButton.jsx
+++ src/components/HamburgerButton.jsx
@@ -10,28 +10,32 @@ const HamburgerButton = ({ isNavOpen, handleIsNavOpen }) => {
                 isNavOpen ? 'Close navigation menu' : 'Open Navigation Menu'
             }
             aria-controls="navigation menu"
-            aria-expanded={isNavOpen ? true : false}>
+            aria-expanded={isNavOpen ? true : false}
+        >
             <span
                 className={classNames(
                     'h-3 w-full bg-white duration-200 ease-linear',
                     {
                         'translate-y-8 -rotate-45': isNavOpen,
                     }
-                )}></span>
+                )}
+            ></span>
             <span
                 className={classNames(
                     'h-3 w-full bg-white duration-200 ease-linear',
                     {
                         'invisible opacity-0': isNavOpen,
                     }
-                )}></span>
+                )}
+            ></span>
             <span
                 className={classNames(
                     'h-3 w-full bg-white duration-200 ease-linear',
                     {
                         '-translate-y-8 rotate-45': isNavOpen,
                     }
-                )}></span>
+                )}
+            ></span>
         </button>
     );
 };
