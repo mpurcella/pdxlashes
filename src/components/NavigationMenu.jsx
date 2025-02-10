2731090f29fb02005c44077e5625a16c6772bdba
Michael Purcella <mjpurcella@gmail.com>
2025-02-09 17:45:05 -0800

    change navigation menu height unit

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -5,7 +5,7 @@ const NavigationMenu = ({ isNavOpen, onClick }) => {
     return (
         <div
             className={classNames(
-                'bg-eggshell fixed top-88 z-10 flex h-[calc(100vh-88px)] w-full justify-center duration-200 ease-in-out md:static md:h-auto md:w-auto md:bg-transparent',
+                'bg-eggshell fixed top-88 z-10 flex h-[calc(100vh-5.5rem)] w-full justify-center duration-200 ease-in-out md:static md:h-auto md:w-auto md:bg-transparent',
                 {
                     'right-0': isNavOpen,
                     'right-full': !isNavOpen,
