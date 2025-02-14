0bb8686d11571e5ac452c3692328090954b8053e
Michael Purcella <mjpurcella@gmail.com>
2025-02-14 13:22:23 -0800

    change hero bg color and opacity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import WomanJpeg from '../assets/images/jpeg/woman.jpeg';
 
 const Hero = () => {
     return (
-        <section className="relative bg-white">
+        <section className="relative bg-black">
             <div className="h-[calc(100vh-4.75rem)]">
                 <picture>
                     <source srcSet={WomanWebp} type="image/webp" />
@@ -11,12 +11,12 @@ const Hero = () => {
                     <img
                         src={WomanJpeg}
                         alt=""
-                        className="h-full w-full object-cover opacity-50"
+                        className="h-full w-full object-cover opacity-10"
                     />
                 </picture>
             </div>
             <div className="absolute top-[10%] left-0 px-28 py-52 md:px-52">
-                <h1 className="font-montserrat lg:text-140 md:text-100 text-60 flex flex-col font-black uppercase">
+                <h1 className="font-montserrat lg:text-140 md:text-100 text-60 flex flex-col font-black text-white uppercase">
                     <span>PDX</span>
                     <span className="-mt-12 md:-mt-20 lg:-mt-28">Lashes</span>
                 </h1>
