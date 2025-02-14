bf1af7e9c613b869a63a123b5e9629838bd4d3e6
Michael Purcella <mjpurcella@gmail.com>
2025-02-14 13:58:26 -0800

    change hero text location on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                     />
                 </picture>
             </div>
-            <div className="absolute top-[10%] left-0 px-28 py-52 md:px-52">
+            <div className="absolute top-[25%] left-0 px-28 py-52 md:top-[10%] md:px-52">
                 <h1 className="font-montserrat lg:text-140 md:text-100 text-60 flex flex-col font-black text-white uppercase">
                     <span>PDX</span>
                     <span className="-mt-12 md:-mt-20 lg:-mt-28">Lashes</span>
