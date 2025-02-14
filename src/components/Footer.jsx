00984b586c63c57fabafa67105c8e3d0ce202efc
Michael Purcella <mjpurcella@gmail.com>
2025-02-13 22:21:14 -0800

    add copyright phosphor icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import SocialList from './SocialList';
+import { Copyright } from '@phosphor-icons/react';
 
 const Footer = () => {
     const year = new Date().getFullYear();
@@ -6,10 +7,10 @@ const Footer = () => {
     return (
         <footer className="flex flex-col items-center gap-40 bg-black p-40 md:flex-row md:justify-between md:px-60 md:py-28">
             <SocialList />
-            <p className="font-inter flex items-center font-light text-white">
-                <span className="text-10">&#9400;</span>
+            <p className="font-inter text-16 flex items-center font-light text-white">
+                <Copyright weight="light" />
                 &nbsp;
-                <span className="text-16">{year} PDX Lashes LLC</span>
+                <span>{year} PDX Lashes LLC</span>
             </p>
         </footer>
     );
